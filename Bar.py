9a7862278cf68519be1ee93bdbbdf2740bc9e180
Martin Weinelt <mweinelt@gmail.com>
2015-03-12 00:41:10 +0100

    Fix duplicate name in Bar class, also add proper signal handler signature

--- Bar.py
+++ Bar.py
@@ -11,7 +11,7 @@ class Bar(object):
     def __init__(self, interval=0.5):
         self.items = []
         self.interval = interval
-        self.pause = False
+        self.paused = False
 
         # signals
         self.signal_pause = signal.SIGSTOP
@@ -38,19 +38,19 @@ class Bar(object):
 
         return json.dumps(results)
 
-    def pause(self):
+    def pause(self, signum, frame):
         """
         Signalhandler for the i3bar stop signal, preventing output
         and calls to BarItem.update() when triggered
         """
-        self.pause = True
+        self.paused = True
 
-    def resume(self):
+    def resume(self, signum, frame):
         """
         Signalhandler for the i3bar continue signal, reallowing output
         and calls to BarItem.update() when triggered
         """
-        self.pause = False
+        self.paused = False
 
     def loop(self):
         log = open('/tmp/py3bar.log', 'w')
@@ -70,7 +70,7 @@ class Bar(object):
         print("[],")  # first item is empty
         while True:
             # out
-            if not self.pause:
+            if not self.paused:
                 print("%s," % self.query())
                 sys.stdout.flush()
 
